c395cf48403aa8ead6d2358cb16a4db5ad15454d
KingAnd7 <109044756+KingAnd7@users.noreply.github.com>
2025-02-14 17:12:07 -0500

    added <stdio.h>

--- GVZork.cpp
+++ GVZork.cpp
@@ -1,5 +1,5 @@
 #include "GVZork.hpp"
 
 int main(int argc, char const *argv[]){
-    
-}
+    printf("hello");
+} 
--- GVZork.hpp
+++ GVZork.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <stdio.h>
 
 class Item{
     public:
