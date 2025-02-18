1affa6fe5f42864390d90627beec3a0d6644e604
KingAnd7 <109044756+KingAnd7@users.noreply.github.com>
2025-02-17 22:44:31 -0500

    test code

--- GVZork.cpp
+++ GVZork.cpp
@@ -1,5 +1,12 @@
 #include "GVZork.hpp"
 
 int main(int argc, char const *argv[]){
-    printf("hello");
+    try {
+        Item rustyNail("Rusty Nail", "A rusty nail (I hope you've had a tetanus shot)", 0, 1);
+        std::cout << rustyNail << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
 } 
