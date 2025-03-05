a56b4917ebb0070eb6920727de1289b633203307
root <root@Rabbitlaptop.>
2025-03-05 10:09:52 -0500

    created some more npcs, don't have full dialog

--- GVZork.cpp
+++ GVZork.cpp
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[]){
         std::cout << rustyNail << std::endl;
 
 //      NPC TEST CODE
+        //wizard test code
         NPC wizard("Merlin", "An old and wise wizard with a long white beard", {}, false);
         wizard.addMessage("Greetings, young adventurer!");
         wizard.addMessage("Beware of the dragon in the northern mountains.");
@@ -13,12 +14,12 @@ int main(int argc, char const *argv[]){
         wizard.addMessage("The stars are aligned for great magic tonight.");
         std::cout << "NPC Name: " << wizard << std::endl;
         std::cout << "Description: " << wizard.getDescription() << std::endl;
-        
-        for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < 10; i++)
         {
             std::cout << "Wizard says: \"" << wizard.getMessage() << "\"" << std::endl;
-        }
-	
+   	}
+
+	//goblin test code
 	NPC goblin("Fartbuckle", "A goblin companion who's yet to find his purpose", {}, true);
 	goblin.addMessage("Hello adventurer.");
 	goblin.addMessage("Fartbuckle would like to come with adventurer.");
@@ -31,6 +32,25 @@ int main(int argc, char const *argv[]){
 		std::cout << goblin << " says: \"" goblin.getMessage() << "\"" << std::endl;
 	}
 
+	//elf test code
+	NPC elf("Heimlin", "An elf who has come to save the day, given payment.", {}, false);
+	elf.addMessage("Greetings traveler.");
+	elf.addMessage("It seems you need my assistance.");
+	elf.addMessage("Bring me payment in the form of calories.");
+	elf.addMessage("If you bring me enough, I will save your school.");
+	std::cout << "NPC Name " << elf << std::endl;
+        std::cout << "description: " << elf.getDescription() << std::endl;
+        for (size_t i = 0; i < 10; i++)
+        {
+                std::cout << elf << " says: \"" elf.getMessage() << "\"" << std::endl;
+        }
+	
+	//ranger test code
+	NPC ranger("Arana", "A drider ranger who's hunting the madman who made the crystal.", {}, false);
+	
+
+	//cleric test code
+	NPC cleric("Lyco", "a werewolf healer who searches for a cure to their own affliction", {}, false);
     return 0;
 };
 };
